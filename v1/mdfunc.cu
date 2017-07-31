9cc33ffa0ef7330d122a42ff4c45cbd05ce01ce4
liuxu <liuxu89@mail.ustc.edu.cn>
2017-07-31 20:24:19 +0800

    fix mdfunc.cu .h

--- v1/mdfunc.cu
+++ v1/mdfunc.cu
@@ -4,13 +4,13 @@
 #define BLOCK_SIZE_512  512 
 #define BLOCK_SIZE_1024 1024 
 
-__managed__ double mfmax;
+__managed__ double gsm_fmax;
+__managed__ double gsm_wili;
 
 
 __global__ void kernel_zero_confv( tpvec *tdconfv, int natom )
     {
-    const int i = threadIdx.x + blockIdx.x * blockDim.x;
-
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
     if ( i < natom )
         {
         tdconfv[i].y = 0.0;
@@ -24,14 +24,13 @@ cudaError_t gpu_zero_confv( tpvec *tdconfv, tpbox tbox )
 
     const int natom = tbox.natom;
 
-    dim3 grids( ceil( natom/block_size ) + 1, 1, 1 );
+    dim3 grids( (natom/block_size)+1, 1, 1 );
     dim3 threads( block_size, 1, 1 );
 
     kernel_zero_confv <<< grids, threads >>> ( tdconfv, natom );
 
     cudaError_t err;
     err = cudaDeviceSynchronize();
-
     if ( err != cudaSuccess )
         {
         fprintf(stderr, "cudaDeviceSync failed, %s, %d, err = %d\n", __FILE__, __LINE__, err);
@@ -41,35 +40,29 @@ cudaError_t gpu_zero_confv( tpvec *tdconfv, tpbox tbox )
     }
 
 
-__global__ void kernel_update_vr( tpvec *tdcon, tpvec *tdconv, tpvec *tdconf, int natom, double dt, double hfdt )
+__global__ void kernel_update_vr( tpvec *tdcon, tpvec *tdconv, tpvec *tdconf, int natom, double dt )
     {
-    const int i = threadIdx.x + blockIdx.x * blockDim.x;
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
 
     if ( i < natom )
-        {
-        double xi, yi;
-        xi = tdcon[i].x;
-        yi = tdcon[i].y;
+        { 
+        double r, v, f;
 
-        double vxi, vyi;
-        vxi = tdconv[i].x;
-        vyi = tdconv[i].y;
+        r = tdcon[i].x;
+        v = tdconv[i].x;
+        f = tdconf[i].x;
+        v += 0.5 * f * dt;
+        r += v * dt;
+        tdconv[i].x = v;
+        tdcon[i].x += r;
 
-        double fxi, fyi;
-        fxi = tdconf[i].x;
-        fyi = tdconf[i].y;
-
-        vxi += fxi * hfdt;
-        vyi += fyi * hfdt;
-
-        xi  += vxi * dt;
-        yi  += vyi * dt;
-
-        tdcon[i].x = xi;
-        tdcon[i].y = yi;
-
-        tdconv[i].x = vxi;
-        tdconv[i].y = vyi;
+        r = tdcon[i].y;
+        v = tdconv[i].y;
+        f = tdconf[i].y;
+        v += 0.5 * f * dt;
+        r += v * dt;
+        tdconv[i].y = v;
+        tdcon[i].y += r;
         }
     }
 
@@ -78,16 +71,14 @@ cudaError_t gpu_update_vr( tpvec *tdcon, tpvec *tdconv, tpvec *tdconf, tpbox tbo
     const int block_size = BLOCK_SIZE_256;
 
     const int natom = tbox.natom;
-    const double hfdt = 0.5 * dt;
 
-    dim3 grids( ceil(natom/block_size)+1, 1, 1 );
+    dim3 grids( (natom/block_size)+1, 1, 1 );
     dim3 threads( block_size, 1, 1 );
 
-    kernel_update_vr <<< grids, threads >>> ( tdcon, tdconv, tdconf, natom, dt, hfdt );
+    kernel_update_vr <<< grids, threads >>> ( tdcon, tdconv, tdconf, natom, dt );
 
     cudaError_t err;
     err = cudaDeviceSynchronize();
-
     if ( err != cudaSuccess )
         {
         fprintf(stderr, "cudaDeviceSync failed, %s, %d, err = %d\n", __FILE__, __LINE__, err);
@@ -97,25 +88,22 @@ cudaError_t gpu_update_vr( tpvec *tdcon, tpvec *tdconv, tpvec *tdconf, tpbox tbo
     }
 
 
-__global__ void kernel_update_v( tpvec *tdconv, tpvec *tdconf, int natom, double tdt, double hfdt )
+__global__ void kernel_update_v( tpvec *tdconv, tpvec *tdconf, int natom, double hfdt )
     {
     const int i = threadIdx.x + blockIdx.x * blockDim.x;
 
     if ( i < natom )
         {
-        double vxi, vyi;
-        vxi = tdconv[i].x;
-        vyi = tdconv[i].y;
+        double v, f;
+        v  = tdconv[i].x;
+        f  = tdconf[i].x;
+        v += f * hfdt;
+        tdconv[i].x = v;
 
-        double fxi, fyi;
-        fxi = tdconf[i].x;
-        fyi = tdconf[i].y;
-
-        vxi += fxi * hfdt;
-        vyi += fyi * hfdt;
-
-        tdconv[i].x = vxi;
-        tdconv[i].y = vyi;
+        v  = tdconv[i].y;
+        f  = tdconf[i].y;
+        v += f * hfdt;
+        tdconv[i].y = v;
         }
     }
 
@@ -126,10 +114,10 @@ cudaError_t gpu_update_v( tpvec *tdconv, tpvec *tdconf, tpbox tbox, double dt)
     const int natom = tbox.natom;
     const double hfdt = 0.5 * dt;
 
-    dim3 grids( ceil(natom/block_size)+1, 1, 1 );
+    dim3 grids( (natom/block_size)+1, 1, 1 );
     dim3 threads( block_size, 1, 1 );
 
-    kernel_update_v <<< grids, threads >>> ( tdconv, tdconf, natom, dt, hfdt );
+    kernel_update_v <<< grids, threads >>> ( tdconv, tdconf, natom, hfdt );
 
     cudaError_t err;
     err = cudaDeviceSynchronize();
@@ -142,60 +130,71 @@ cudaError_t gpu_update_v( tpvec *tdconv, tpvec *tdconf, tpbox tbox, double dt)
     return err;
     }
 
-__global__ void kernel_calc_force( tplist *tdlist, tpvec *tdcon, double *tddradius, tpvec *tdconf, int natom, double lx, double ly )
+
+__global__ void kernel_calc_force( tplist *tdlist, tpvec *tdcon, double *tdradius, tpvec *tdconf, int natom, double lx, double ly )
     {
+    __shared__ double sm_wili;
+
     const int i = threadIdx.x + blockIdx.x * blockDim.x;
+
     if ( i >= natom )
         return;
 
-    int nbsum;
-    nbsum = tdlist[i].nbsum;
+    if ( i == 0 ) sm_wili = 0.0;
 
-    double fxi, fyi;
-    fxi = 0.0;
-    fyi = 0.0;
+    int nbsum = tdlist[i].nbsum;
 
-    double xi, yi, ri;
-    xi = tdcon[i].x;
-    yi = tdcon[i].y;
-    ri = tddradius[i];
+    double fxi   = 0.0;
+    double fyi   = 0.0;
+    double wilii = 0.0;
+
+    double xi = tdcon[i].x;
+    double yi = tdcon[i].y;
+    double ri = tdradius[i];
 
     for ( int jj=0; jj<nbsum; jj++ )
         {
-        int j;
-        j = tdlist[i].nb[jj];
+        int j = tdlist[i].nb[jj];
 
-        double xj, yj, rj;
-        xj = tdcon[j].x;
-        yj = tdcon[j].y;
-        rj = tddradius[j];
+        double xj  = tdcon[j].x;
+        double yj  = tdcon[j].y;
+        // dij equal to raidius of atom j
+        double dij = tdradius[j];
 
-        double xij, yij;
-        xij = xj - xi;
-        yij = yj - yi;
-        xij = xij - round(xij/lx)*lx;
-        yij = yij - round(yij/ly)*ly;
+        // xij
+        xj -= xi;
+        yj -= yi;
+        xj -= round(xj/lx)*lx;
+        yj -= round(yj/ly)*ly;
 
-        double rij, dij;
-        rij = xij*xij + yij*yij; // rij2
-        dij = ri + rj;
+        double rij;
+        rij = xj*xj + yj*yj; // rij2
+        dij = ri + dij;
 
         if ( rij < dij*dij )
             {
             rij = sqrt(rij);
             
-            double fr; // wij
-            fr = ( 1.0 - rij/dij ) / dij / rij;
+            //double fr  - > dij
+            dij = ( 1.0 - rij/dij ) / dij / rij;
 
-            fxi -= fr * xij;
-            fyi -= fr * yij;
+            // wili
+            wilii += dij * rij*rij; 
+
+            fxi -= dij * xj;
+            fyi -= dij * yj;
             }
         }
     tdconf[i].x = fxi;
     tdconf[i].y = fyi;
+
+    atomicAdd( &sm_wili, wilii );
+    __syncthreads();
+    atomicAdd( &gsm_wili, sm_wili );
+
     }
 
-cudaError_t gpu_calc_force( tplist *tdlist, tpvec *tdcon, double *tddradius, tpvec *tdconf, tpbox tbox )
+cudaError_t gpu_calc_force( tplist *tdlist, tpvec *tdcon, double *tdradius, tpvec *tdconf, double *static_press, tpbox tbox )
     {
     const int block_size = BLOCK_SIZE_256;
 
@@ -203,10 +202,12 @@ cudaError_t gpu_calc_force( tplist *tdlist, tpvec *tdcon, double *tddradius, tpv
     const double lx = tbox.x;
     const double ly = tbox.y;
 
-    dim3 grids( ceil(natom/block_size)+1, 1, 1 );
+    gsm_wili = 0.0;
+
+    dim3 grids( (natom/block_size)+1, 1, 1 );
     dim3 threads( block_size, 1, 1 );
 
-    kernel_calc_force <<< grids, threads >>>( tdlist, tdcon, tddradius, tdconf, natom, lx, ly );
+    kernel_calc_force <<< grids, threads >>>( tdlist, tdcon, tdradius, tdconf, natom, lx, ly );
 
     cudaError_t err;
     err = cudaDeviceSynchronize();
@@ -216,8 +217,11 @@ cudaError_t gpu_calc_force( tplist *tdlist, tpvec *tdcon, double *tddradius, tpv
         fprintf(stderr, "cudaDeviceSync failed, %s, %d, err = %d\n", __FILE__, __LINE__, err);
         }
 
+    *static_press = gsm_wili / 2.0 / 2.0 / lx / ly;
+
     return err;
     }
+
 
 __global__ void kernel_calc_fmax( tpvec *tdconf, int natom )
     {
@@ -248,7 +252,7 @@ __global__ void kernel_calc_fmax( tpvec *tdconf, int natom )
 
     if ( tid == 0 )
         {
-        atomicMax( &mfmax, block_f[0] );
+        atomicMax( &gsm_fmax, block_f[0] );
         }
     }
 
@@ -258,7 +262,9 @@ double gpu_calc_fmax( tpvec *tdconf, tpbox tbox )
 
     const int natom = tbox.natom;
 
-    dim3 grids( ceil(natom/block_size)+1, 1, 1);
+    gsm_fmax = 0.0;
+
+    dim3 grids( (natom/block_size)+1, 1, 1);
     dim3 threads( block_size, 1, 1);
 
     kernel_calc_fmax <<< grids, threads >>> ( tdconf, natom );
@@ -271,5 +277,7 @@ double gpu_calc_fmax( tpvec *tdconf, tpbox tbox )
         fprintf(stderr, "cudaDeviceSync failed, %s, %d, err = %d\n", __FILE__, __LINE__, err);
         }
 
-    return mfmax;
+    return gsm_fmax;
     }
+
+
--- v1/mdfunc.h
+++ v1/mdfunc.h
@@ -17,6 +17,7 @@
 cudaError_t gpu_zero_confv( tpvec *tdconfv, tpbox tbox );
 cudaError_t gpu_update_vr( tpvec *tdcon, tpvec *tdconv, tpvec *tdconf, tpbox tbox, double dt);
 cudaError_t gpu_update_v( tpvec *tdconv, tpvec *tdconf, tpbox tbox, double dt);
+cudaError_t gpu_calc_force( tplist *tdlist, tpvec *tdcon, double *tddradius, tpvec *tdconf, double *static_press, tpbox tbox );
 double gpu_calc_fmax( tpvec *tdconf, tpbox tbox );
 
 #endif
